0c21f3ff5a8d5d6fedc68ddbb6c1727e31780391
srikanth <sri@123>
2023-10-05 12:22:28 +0530

    tasktup

---
+++ tasktup.py
@@ -0,0 +1,64 @@
+#1.Write a Python program to unpack a tuple into several variables.
+#emp = ("srikanth",22,"python",30000)
+#name,age,role,salary = emp 
+#print(emp)
+#print(role)
+#print(name)
+#print(age)
+
+#2.Write a Python program to add an item to a tuple.
+
+#tuple = ("srikanth","guntur",22,)
+#list_1 = list(tuple)
+#list_1.append("hyderabad")
+#print(list_1)
+
+#3.Write a Python program to convert a tuple to a string.
+ #Ex:tup = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+
+#given_1 =  ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+#string_1 = tuple(given_1)
+#print(string_1)
+
+#4.Write a Python program to get the specified element from the last 
+#tuple_1 = ("ramu","srinu",22,23)
+#print(tuple_1[3])
+
+#5.Write a Python progra"m to add member(s) to a set.
+#set_1 = {"lion","tiger","cheetha"}
+#set_1.add("sheer")
+#print(set_1)
+
+#6.Write a Python program to create an intersection of sets.
+#set_1 = {1,2,4,2}
+#set_2 = {2,5,6,4}
+#inter_1 = set_1 & set_2
+#print(inter_1)
+
+
+#7.Write a Python program to create a union of sets.
+#set_1 = {1,2,3,4,5}
+#set_2 = {6,7,8,9} 
+#union_1 = set_1|set_2
+#print(union_1)
+
+#8.Write a Python program to create set difference.
+#set_1 = {1,2,3,4,5}
+#set_2 = {6,7,8,9} 
+#diff = set_1 - set_2 
+#print(diff)
+
+
+#9.Write a Python program to create a symmetric difference.
+#set_1 = {1,2,3,4,5}
+#set_2 = {6,7,8,5,9} 
+#sey_1 = set_1 ^ set_2 
+#print(sey_1)
+
+#10.Write a Python program to find the maximum and minimum values in a set.
+#set_1 = {1,2,3,4,5}
+#set_2 = {6,7,8,9} 
+#maximum_1 = max(set_1)
+#minimum_1 = min(set_2)
+#print(maximum_1)
+#print(minimum_1)
